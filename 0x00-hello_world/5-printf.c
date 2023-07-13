b7158b5d981a694d47258ce13ac8321acccb230e
nanak101 <nanakwakudarkwah@.com>
2023-07-13 16:52:55 -0700

    using printf to print to the standard output

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
-  *main - Entry point
-  *Return: always 0 (success)
-  */
+ *main - Entry point
+ *
+ *Return: always (0)
+ *
+ */
 int main(void)
 {
-	printf("with proper grammar, but th outcome is a piece of art ,\n");
+	printf("with proper grammar, but th outcome is a piece of art,\n");
 	return (0);
