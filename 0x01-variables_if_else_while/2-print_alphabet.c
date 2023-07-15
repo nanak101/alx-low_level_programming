09f61877626a3d882ab2d76776fcfd25d6dc60df
nanak101 <nanakwakudarkwah@.com>
2023-07-15 09:38:00 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ * main - Prints the alphabetic
+ * Return: (Success)
+ */
+
+
+int main(void)
+
+
+{
+	char C;
+
+	for (C = 'a'; C <= 'z'; C++)
+	putchar(C);
+	putchar('\n');
+	return (0);
+
+}
