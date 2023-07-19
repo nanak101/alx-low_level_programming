4d8c1113f04ce1e9ce7cd4433c29c328e398e095
nanak101 <nanakwakudarkwah@.com>
2023-07-18 18:06:36 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,11 +10,11 @@ int main(void)
 	int 1;
 	unsigned long int j, k, next, sum;
 
-	j = 1
+	j = 1;
 	k = 2;
 	sum = 0;
 
-	for (i = 1; i <= 33; ++1)
+	for (i = 1; i <= 33; ++i)
 	{
 		if (j < 4000000 && (j % 2) == 0)
 		{
