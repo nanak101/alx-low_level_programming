56f65f76e4e53a548db32678b566c4c615e95f48
nanak101 <nanakwakudarkwah@.com>
2023-07-28 05:15:15 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,6 +4,7 @@
 /**
  * rot13 - encoder rot13
  * @s: pointer to string params
+ *
  * Return: *s
  */
 
@@ -11,7 +12,7 @@ char *rot13(char *s)
 {
 	int i;
 	int j;
-	char data[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	for (i = 0; s[i] != '\0'; i++)
 	{
