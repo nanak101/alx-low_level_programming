db6ec17de30af739fafa54a88eccdb2feafec932
nanak101 <nanakwakudarkwah@.com>
2023-08-14 14:11:39 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file
- *
- * Return: Always 0 (success)
+ * main - main function
+ * Return: 0
  */
+
 int main(void)
 {
-	printf("%s\n". __FILE__);
+	printf("%s\n", __FILE__);
+
 	return (0);
 }
