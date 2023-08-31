61557b47ac252811cc0304ba12baa37665a54f0d
nanak101 <nanakwakudarkwah@.com>
2023-08-31 08:45:53 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,8 +9,8 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
-		return (-1)
+	if (index >= (sizeof(unsigned long int) * 8)
+		return (-1);
 
 	*n = (~(1UL << index) & *n);
 	return (1);
